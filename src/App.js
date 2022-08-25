d2426a07c13fa9029b3cc3a73fec454cd85f17e6
chuxyz <ikpeamachukwudi@gmail.com>
2022-08-25 18:44:44 +0100

    Add HomeScreen and load dummy data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Homescreen from './screens/HomeScreen'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Hello</h1>
+          <Homescreen/>
         </Container>
       </main>
       <Footer/>
