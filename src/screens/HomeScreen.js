160b82a9ee6f9dc5db27119c0334ea0536eb300c
chuxyz <ikpeamachukwudi@gmail.com>
2022-08-25 23:41:15 +0100

    add and layout product component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
 
 import products from '../products'
 
@@ -8,9 +9,9 @@ function HomeScreen() {
       <div>
           <h1>Latest products</h1>
           <Row>
-              {products.map(product => (
-                  <Col sm={12} md={6} lg={4} xl={3}>
-                      <h3>{product.name}</h3>
+              {products.map((product) => (
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                      <Product product={product} />
                   </Col>
               ))}
           </Row>
